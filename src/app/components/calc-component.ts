4a99425a98f33dac4413692711f709a73b3d6038
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-08-23 13:52:21 -0700

    Calculator app is functioning

--- src/app/components/calc-component.ts
+++ src/app/components/calc-component.ts
@@ -8,10 +8,11 @@ import { Component } from '@angular/core';
       <div class="input">
         <div class="input-group mb-3">
           <input
-            id="inputOne"
+            [id]="inputOne"
             class="form-control"
             type="text"
             placeholder="first number"
+            [(ngModel)]="inputOne"
           />
         </div>
         <div class="input-group mb-3">
@@ -20,6 +21,7 @@ import { Component } from '@angular/core';
             class="form-control"
             type="text"
             placeholder="second number"
+            [(ngModel)]="inputTwo"
           />
         </div>
       </div>
@@ -28,44 +30,71 @@ import { Component } from '@angular/core';
     <div
       class="operations d-grid gap-2 d-md-block d-md-flex justify-content-md-center"
     >
-      <button
-        type="button"
-        class="btn btn-primary btn-lg"
-        data-operation="add"
-        data-type="button"
-      >
+      <button type="button" class="btn btn-primary btn-lg" (click)="add()">
         +
       </button>
-      <button
-        type="button"
-        class="btn btn-primary btn-lg"
-        data-operation="sub"
-        data-type="button"
-      >
+      <button type="button" class="btn btn-primary btn-lg" (click)="sub()">
         -
       </button>
-      <button
-        type="button"
-        class="btn btn-primary btn-lg"
-        data-operation="mult"
-        data-type="button"
-      >
+      <button type="button" class="btn btn-primary btn-lg" (click)="mult()">
         x
       </button>
-      <button
-        type="button"
-        class="btn btn-primary btn-lg"
-        data-operation="div"
-        data-type="button"
-      >
+      <button type="button" class="btn btn-primary btn-lg" (click)="div()">
         /
       </button>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <h3>Answer:</h3>
+        <p id="answer">{{ calcAnswer }}</p>
+      </div>
+      <div class="history col-md-6">
+        <h3>History</h3>
+        <ul id="calcHist">
+          <li *ngFor="let c of calcHistory">{{ c }}</li>
+        </ul>
+      </div>
     </div>
   `,
   selector: 'calculator',
 })
 export class CalculatorComponent {
+  answer: string = '';
+  inputOne: number = 0;
+  inputTwo: number = 0;
+  symbol: string = '';
 
-  
+  calcAnswer: number = 0;
+  calcHistory: Array<string> = [];
 
+  // this.inputOne +
+
+  add(): void {
+    this.calcAnswer = Number(this.inputOne) + Number(this.inputTwo);
+    this.answer =
+      this.inputOne + ' + ' + this.inputTwo + ' = ' + this.calcAnswer;
+    this.calcHistory.unshift(this.answer);
+  }
+
+  sub(): void {
+    this.calcAnswer = Number(this.inputOne) - Number(this.inputTwo);
+    this.answer =
+      this.inputOne + ' - ' + this.inputTwo + ' = ' + this.calcAnswer;
+    this.calcHistory.unshift(this.answer);
+  }
+
+  mult(): void {
+    this.calcAnswer = Number(this.inputOne) * Number(this.inputTwo);
+    this.answer =
+      this.inputOne + ' * ' + this.inputTwo + ' = ' + this.calcAnswer;
+    this.calcHistory.unshift(this.answer);
+  }
+
+  div(): void {
+    this.calcAnswer = Number(this.inputOne) / Number(this.inputTwo);
+    this.answer =
+      this.inputOne + ' / ' + this.inputTwo + ' = ' + this.calcAnswer;
+    this.calcHistory.unshift(this.answer);
+  }
 }
